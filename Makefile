86ec10e611c6b70a87f670a78bdd510765a81f48
重劍無鋒 <tecpoirot@foxmail.com>
2023-12-17 14:55:07 +0800

    build: 完善 Makefile，增加构建脚本。

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 
+release: all_doc compile
+
+release_rebuild: all_doc build
+
 prepare_doc: gen_redoc gen_swagger gen_rapidoc
 
 convert_json_yaml:
@@ -6,10 +10,20 @@ convert_json_yaml:
 
 gen_redoc: convert_json_yaml
 	npx @redocly/cli build-docs api/openapi-spec/HTTPBinGo.openapi.yaml -o cmd/httpbingo/doc/html/redoc.html
-	open -R cmd/httpbingo/doc/html/redoc.html
+#open -R cmd/httpbingo/doc/html/redoc.html
 
 gen_swagger:
-	cp api/HTTPBinGo.html cmd/httpbingo/doc/html/swagger.html
+	cat api/HTTPBinGo.html | sed 's|<title>Apifox 接口文档</title>|<title>HTTPBinGo 使用说明</title>|g' > cmd/httpbingo/doc/html/swagger.html
 
 gen_rapidoc:
 	cp api/openapi-spec/HTTPBinGo.openapi.yaml cmd/httpbingo/doc/html/rapidoc/specs/temp.yaml
+
+all_doc: prepare_doc convert_json_yaml gen_redoc gen_swagger gen_rapidoc
+
+build:
+	bash scripts/build.sh httpbingo ./cmd/httpbingo -a -v
+
+compile:
+	bash scripts/build.sh httpbingo ./cmd/httpbingo -v
+
+.PHONY: build compile
---
+++ scripts/build.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+BASE_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+readonly BASE_DIR
+
+parse_platform () {
+    local platform
+
+    platform=${1:-$(go version | awk '{ print $4 }')}
+
+    os=$(echo "${platform}" | awk -F '/' '{ print $1 }')
+    arch=$(echo "${platform}" | awk -F '/' '{ print $2 }')
+
+#    echo "$os" "$arch"
+
+    export CGO_ENABLED=0
+    export GOOS=$os
+    export GOARCH=$arch
+}
+
+gen_bin_file () {
+    echo "${APP_NAME}_${GOOS}_${GOARCH}"
+}
+
+
+do_build () {
+    local output_file
+    output_file=$(realpath "$BASE_DIR/../build")/$(gen_bin_file)
+    local cmd_line=(go build --ldflags="-s -w" "$@" -o "$output_file" "$MODULE_PATH")
+    echo "[ OS: $os  Architechture: $arch ]"
+    echo "${cmd_line[@]}"
+    "${cmd_line[@]}"
+}
+
+
+# $1 应用名称
+# $2 模块路径
+main () {
+    APP_NAME=${1:-app}
+    MODULE_PATH=${2:-'./cmd'}
+
+    source "$BASE_DIR/supported_platforms.sh"
+
+    shift 2
+    for platform in "${SUPPORTED_PLATFORMS_LIST[@]}"; do
+        parse_platform "$platform"
+        do_build "$@"
+    done
+}
+
+main "$@"
